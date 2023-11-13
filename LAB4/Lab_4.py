f06512c799dd5ab7be469fd088bb99f61c970df4
iskander17Q <forisc@mail.ru>
2023-11-13 10:38:29 +0200

    Modification 2

--- LAB4/Lab_4.py
+++ LAB4/Lab_4.py
@@ -4,34 +4,6 @@ import random
 import csv
 from PyQt5 import QtWidgets, QtGui
 
-# Папка, куда будет создан файл-аннотации
-annotation_folder = 'C:\\Users\\admin\\Desktop\\UTM\\PD\\lab_4\\'
-annotation_file = os.path.join(annotation_folder, 'annotation.csv')
-
-# Функция для создания аннотации
-def create_annotation_file(source_folder, class_name):
-    with open('annotations.csv', 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(['C:\\Users\\admin\\Desktop\\UTM\\PD\\lab_3\\dataset2', '..\\lab_2\\dataset', 'rose'])
-
-        for root, dirs, files in os.walk(source_folder):
-            for file in files:
-                if file.endswith('.jpg'):
-                    absolute_path = os.path.join(root, file)
-                    relative_path = os.path.relpath(absolute_path, annotation_folder)
-                    label = os.path.basename(absolute_path)
-                    writer.writerow([absolute_path, relative_path, label])
-
-def get_next_instance(class_label, dataset_path):
-    class_files = [f for f in os.listdir(dataset_path) if f.startswith(class_label)]
-    if class_files:
-        random.shuffle(class_files)
-        return os.path.join(dataset_path, class_files.pop())
-    else:
-        return None
-
-
-
 class DatasetApp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -39,37 +11,30 @@ class DatasetApp(QtWidgets.QWidget):
         self.initUI()
 
     def initUI(self):
-        # Определите интерфейс
-
         self.setWindowTitle('Dataset App')
         self.setGeometry(100, 100, 1300, 550)
-        self.folderpath = None  # Переменная для хранения пути к папке с исходным датасетом
+        self.folderpath = None
 
-        # Кнопка для выбора папки с исходным датасетом
-        self.browse_button = QtWidgets.QPushButton('Выбрать папку с датасетом', self)
-        self.browse_button.setGeometry(20, 20, 200, 30)
-        self.browse_button.clicked.connect(self.browse_dataset_folder)
+        self.browse_button = self.create_button('Выбрать папку с датасетом', self.browse_dataset_folder, 20, 20)
+        self.create_annotation_button = self.create_button('Создать аннотацию', self.create_annotation, 20, 60)
+        self.create_annotation_button.setEnabled(False)
 
-        # Кнопка для создания файла аннотации
-        self.create_annotation_button = QtWidgets.QPushButton('Создать аннотацию', self)
-        self.create_annotation_button.setGeometry(20, 60, 200, 30)
-        self.create_annotation_button.setEnabled(False)  # По умолчанию отключена
-        self.create_annotation_button.clicked.connect(self.create_annotation)
+        self.next_class1_button = self.create_button('Следующий класс 1', self.get_next_class1, 20, 100)
+        self.next_class2_button = self.create_button('Следующий класс 2', self.get_next_class2, 20, 140)
 
-        # Кнопки для получения следующего экземпляра
-        self.next_class1_button = QtWidgets.QPushButton('Следующий класс 1', self)
-        self.next_class1_button.setGeometry(20, 100, 200, 30)
-        self.next_class1_button.setEnabled(False)  # По умолчанию отключена
-        self.next_class1_button.clicked.connect(self.get_next_class1)
+        self.image_label = self.create_label(250, 20, 1000, 500)
 
-        self.next_class2_button = QtWidgets.QPushButton('Следующий класс 2', self)
-        self.next_class2_button.setGeometry(20, 140, 200, 30)
-        self.next_class2_button.setEnabled(False)  # По умолчанию отключена
-        self.next_class2_button.clicked.connect(self.get_next_class2)
+    def create_button(self, text, on_click, x, y):
+        button = QtWidgets.QPushButton(text, self)
+        button.setGeometry(x, y, 200, 30)
+        button.clicked.connect(on_click)
+        return button
 
-        self.image_label = QtWidgets.QLabel(self)
-        self.image_label.setGeometry(250, 20, 1000, 500)  # Изменили размеры здесь
-        self.image_label.setScaledContents(True)
+    def create_label(self, x, y, width, height):
+        label = QtWidgets.QLabel(self)
+        label.setGeometry(x, y, width, height)
+        label.setScaledContents(True)
+        return label
 
     def browse_dataset_folder(self):
         self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Выберите папку с исходным датасетом')
@@ -80,20 +45,40 @@ class DatasetApp(QtWidgets.QWidget):
 
     def create_annotation(self):
         if self.folderpath:
-            create_annotation_file(self.folderpath, 'rose')
-            create_annotation_file(self.folderpath, 'tulip')
-            pass
+            self.create_annotations(['rose', 'tulip'])
+
+    def create_annotations(self, class_names):
+        with open('annotations.csv', 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['Absolute Path', 'Relative Path', 'Label'])
+
+            for class_name in class_names:
+                class_path = os.path.join(self.folderpath, class_name)
+                for root, _, files in os.walk(class_path):
+                    for file in files:
+                        if file.endswith('.jpg'):
+                            absolute_path = os.path.join(root, file)
+                            relative_path = os.path.relpath(absolute_path, self.folderpath)
+                            writer.writerow([absolute_path, relative_path, class_name])
+
+    def get_next_instance(self, class_label):
+        class_files = [f for f in os.listdir(os.path.join(self.folderpath, class_label)) if f.endswith('.jpg')]
+        if class_files:
+            random.shuffle(class_files)
+            return os.path.join(self.folderpath, class_label, class_files.pop())
+        else:
+            return None
 
     def get_next_class1(self):
         if self.folderpath:
-            next_instance = get_next_instance('rose', self.folderpath)
+            next_instance = self.get_next_instance('rose')
             if next_instance:
                 pixmap = QtGui.QPixmap(next_instance)
                 self.image_label.setPixmap(pixmap)
 
     def get_next_class2(self):
         if self.folderpath:
-            next_instance = get_next_instance('tulip', self.folderpath)
+            next_instance = self.get_next_instance('tulip')
             if next_instance:
                 pixmap = QtGui.QPixmap(next_instance)
                 self.image_label.setPixmap(pixmap)
